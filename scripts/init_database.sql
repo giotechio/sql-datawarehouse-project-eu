3c0b3d965d63c560ea05a9db3443354d371c43a4
giotechio <125731496+giotechio@users.noreply.github.com>
2025-09-22 15:38:48 +0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -6,11 +6,13 @@ Create Database and Schemas
 
 Script Purpose:
   This script creates a database named "DWHEU" after cheking if it already exists.
-If the database exists , it will be droped and recreated. 
+!! If the database exists , it will be droped and recreated !!
+
 The script creates three schemas within the database, named: 'bronze','silver'and 'gold'.
 
 !! Warning !!
-  This script will permanently drop the database "DWHEU". All the data contained in this 
+  This script will permanently drop the database "DWHEU". 
+All the data contained in this 
 database will be permanently deleted. 
 If you need the data , make a copy or a backup before running this script.
 */
