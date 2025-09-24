86967608a42886af0bbefe530461ed315d3b50b5
giotechio <125731496+giotechio@users.noreply.github.com>
2025-09-24 11:24:36 +0400

    Update load_bronze_procedure.sql

--- scripts/bronze/load_bronze_procedure.sql
+++ scripts/bronze/load_bronze_procedure.sql
@@ -17,89 +17,155 @@ Usage Example:
 <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
-BEGIN
-	BEGIN TRY 
-		
-		TRUNCATE TABLE bronze.car_eu_car_sales;
-		BULK INSERT bronze.car_eu_car_sales
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\car\eu_car_sales.csv'
-		WITH (
-			FIRSTROW =2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-			);
+-- Create a Stored Procedure for bronze layer
 
-		TRUNCATE TABLE bronze.econ_eu_country_data;
-		BULK INSERT bronze.econ_eu_country_data
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\econ\eu_country_data.csv'
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+
+-- Timing of Loadings
+DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+
+	-- ERROR HANDLING 'TRY-CATCH'
+	BEGIN TRY
+SET @batch_start_time = GETDATE();
+		PRINT '';
+		PRINT '====== Loading Bronze Layer =====';
+
+		--	Tables for CAR
+
+SET @start_time = GETDATE();
+		PRINT '';
+		PRINT '====== Loading CAR Table =====';
+		-- first empty the table
+		TRUNCATE TABLE bronze.car_eu_car_sales;
+		-- load tyhe data in empty table
+		BULK INSERT bronze.car_eu_car_sales
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/car/eu_car_sales.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
-			);
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
 
+SET @start_time = GETDATE();
+		--Tables for ECON
+		PRINT '';
+		PRINT '====== Loading ECON Tables =====';
+		-- first empty the table
+		TRUNCATE TABLE bronze.econ_eu_country_data;
+		-- load tyhe data in empty table
+		BULK INSERT bronze.econ_eu_country_data
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/econ/eu_country_data.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
 
+SET @start_time = GETDATE();
+		-- first empty the table
 		TRUNCATE TABLE bronze.econ_eu_gdp_capita;
+		-- load tyhe data in empty table
 		BULK INSERT bronze.econ_eu_gdp_capita
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\econ\eu_gdp_capita.csv'
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/econ/eu_gdp_capita.csv'
 		WITH (
-			FIRSTROW =2,
+			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
-			);
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';	
 
+SET @start_time = GETDATE();
+		 -- first empty the table
 		TRUNCATE TABLE bronze.econ_eu_inflation;
+		-- load tyhe data in empty table
 		BULK INSERT bronze.econ_eu_inflation
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\econ\eu_inflation.csv'
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/econ/eu_inflation.csv'
 		WITH (
-			FIRSTROW =2,
+			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
-			);
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
 
+SET @start_time = GETDATE();
+		 -- first empty the table
 		TRUNCATE TABLE bronze.econ_eu_population;
+		-- load tyhe data in empty table
 		BULK INSERT bronze.econ_eu_population
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\econ\eu_population.csv'
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/econ/eu_population.csv'
 		WITH (
-			FIRSTROW =2,
+			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
-			);
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
 
-		TRUNCATE TABLE bronze.nrg_eu_fuel_prices;
-		BULK INSERT bronze.nrg_eu_fuel_prices
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\nrg\eu_fuel_prices.csv'
-		WITH (
-			FIRSTROW =2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-			);
-
-		TRUNCATE TABLE bronze.nrg_eu_electricity_prices;
-		BULK INSERT bronze.nrg_eu_electricity_prices
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\nrg\eu_electricity_prices.csv'
-		WITH (
-			FIRSTROW =2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-			);
-
+SET @start_time = GETDATE();
+		-- Tables for NRG
+		PRINT '';
+		PRINT '====== Loading NRG Tables =====';
+		-- first empty the table
 		TRUNCATE TABLE bronze.nrg_eu_electricity_generation;
+		-- load tyhe data in empty table
 		BULK INSERT bronze.nrg_eu_electricity_generation
-		FROM 'C:\Users\My Computer\Desktop\PROJECT_1\nrg\eu_electricity_generation.csv'
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/nrg/eu_electricity_generation.csv'
 		WITH (
-			FIRSTROW =2,
+			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
 			TABLOCK
-			);
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
+
+SET @start_time = GETDATE();
+		-- first empty the table
+		TRUNCATE TABLE bronze.nrg_eu_electricity_prices;
+		-- load tyhe data in empty table
+		BULK INSERT bronze.nrg_eu_electricity_prices
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/nrg/eu_electricity_prices.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
+
+SET @start_time = GETDATE();
+		-- first empty the table
+		TRUNCATE TABLE bronze.nrg_eu_fuel_prices;
+		-- load tyhe data in empty table
+		BULK INSERT bronze.nrg_eu_fuel_prices
+		FROM 'C:\Users\My Computer\Desktop\PROJECT_1/nrg/eu_fuel_prices.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+SET @end_time = GETDATE();
+PRINT'>> Load Duration : '+CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR)+'sec';
+
+--Batch timing
+SET @batch_end_time = GETDATE();
+PRINT''
+PRINT'Loading bronze Layer finished'
+PRINT'>>| Total Load Duration : '+CAST(DATEDIFF(second, @batch_start_time, @batch_end_time) AS NVARCHAR)+'sec';
+
 
 	END TRY
-	BEGIN CATCH -- display the error messages
-		PRINT ' - - - - - - - - - - - - - - '
-		PRINT 'Error MEssage' + ERROR_MESSAGE();
-		PRINT 'Error MEssage' + CAST(ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error MEssage' + CAST(ERROR_STATE() AS NVARCHAR);
+	BEGIN CATCH
+		PRINT '== ERROR OCCURED DURING LOADING bronze Layer ';
+		PRINT 'Error Message'+ ERROR_MESSAGE();
+		PRINT 'Error Message'+ CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message'+ CAST(ERROR_STATE() AS NVARCHAR);
 	END CATCH
+END
 
-END -- end of stored procedure
